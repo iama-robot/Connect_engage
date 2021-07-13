f9ddeee36d6c555959029dfe266d79e9643dda3b
Vasundhara Rai <63060137+iama-robot@users.noreply.github.com>
2021-07-13 12:27:08 +0530

    Update firebase.js

--- Chat Setup/src/firebase.js
+++ Chat Setup/src/firebase.js
@@ -2,13 +2,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCmKKRs4AZG0WtJ81uOzIYwC3VQee8OwdQ",
-    authDomain: "connect-app-bac80.firebaseapp.com",
-    projectId: "connect-app-bac80",
-    storageBucket: "connect-app-bac80.appspot.com",
-    messagingSenderId: "815274637737",
-    appId: "1:815274637737:web:4f2dd4002e26862ead2cb8",
-    measurementId: "G-75M46JW3B4"
+    // your config here
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
